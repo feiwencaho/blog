1fb8a5467097f2635905f8d80d0240f87e6ffc93
fei <wenchaof@163.com>
2015-11-21 21:56:13 +0800

    引入bootstrap,前段首页大致完成,后台orm完成

--- orm/blog.py
+++ orm/blog.py
@@ -1,38 +1,39 @@
 #coding=utf8
+
 '''
 Created on 2015年10月4日
 @author: fei
 '''
-from orm.models import Category,User, Blog
+from orm.models import Category, User, Blog
 
 class UserOrm():
-    def __init__(self,session):
+    def __init__(self, session):
         self.session = session
         
-    def findUserById(self,user_id):
+    def findUserById(self, user_id):
         return self.session.query(User).filter_by(id=user_id).one()
     
-    def findUserByUser(self,user):
+    def findUserByUser(self, user):
         return self.session.query(User).filter_by(username=user.username,password=user.password).first()
     
 class CategoryOrm():
-    def __init__(self,session):
+    def __init__(self, session):
         self.session = session
         
     def findAllCategories(self):
         return self.session.query(Category).all()
 
-    def addCategory(self,Category):
+    def addCategory(self, Category):
         self.session.add(Category)
 
 class BlogOrm():
-    def __init__(self,session):
+    def __init__(self, session):
         self.session = session
 
-    def findBlogsByUser(self,user_id):
+    def findBlogsByUser(self, user_id):
         return self.session.query(Blog).filter(user_id=user_id).all()
     
-    def addBlog(self,Blog):
+    def addBlog(self, Blog):
         self.session.add(Blog)
     
         
